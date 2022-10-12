e926ba3058b86266a8f7242cc3ecd3ed33c18ca6
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-10-12 16:42:57 -0300

    Padrão de projeto com Page Objects

--- cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
+++ cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -1,4 +1,4 @@
-
+import SignupPage from '../pages/SignupPage'
 
 describe('cadastro', ()=>{
     it('Usuário deve ser tornar um entregador', ()=>{
@@ -25,41 +25,19 @@ describe('cadastro', ()=>{
             cnh:'cnh-digital.jpg'
         }
 
-        cy.get('input[ name="name"]').type(deliver.name)
-        cy.get('input[ name="cpf"]').type(deliver.cpf)
-        cy.get('input[ name="email"]').type(deliver.email)
-        cy.get('input[ name="whatsapp"]').type(deliver.whatsapp)
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-        cy.get('input[type=button][value="Buscar CEP"]').click()
-        cy.get('input[name="address-number"]').type(deliver.address.number)
-        cy.get('input[name="address-details"]').type(deliver.address.details)
+        signup.go()
+        signup.fillform(deliver)
+        signup.submit()
 
-        cy.get('input[name="address"]').should('have.value', deliver.address.street)
-        cy.get('input[name="district"]').should('have.value', deliver.address.district)
-        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click()
-
-        cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-
-            cy.get('form button[type="submit"]').click()
-
-            const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-            cy.get('.swal2-container .swal2-html-container')
-                .should('have.text', expectedMessage)
-                
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage) 
 
         })
 
         it('CPF incorreto', ()=>{
-            cy.viewport(1440, 900)
-            cy.visit('https://buger-eats.vercel.app')
-    
-            cy.get('a[href="/deliver"]').click()
-            cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
-    
+              
             var deliver = {
                 name: 'João Ricardo',
                 cpf: '123456789aa',
@@ -76,29 +54,15 @@ describe('cadastro', ()=>{
                 delivery_method:'Moto',
                 cnh:'cnh-digital.jpg'
             }
+            var signup = new SignupPage()
+
+            signup.go()
+            signup.fillform(deliver)
+            signup.submit()
+            signup.alertMessageShoulBe('Oops! CPF inválido')
+
     
-                cy.get('input[ name="name"]').type(deliver.name)
-                cy.get('input[ name="cpf"]').type(deliver.cpf)
-                cy.get('input[ name="email"]').type(deliver.email)
-                cy.get('input[ name="whatsapp"]').type(deliver.whatsapp)
-    
-                cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
-                cy.get('input[type=button][value="Buscar CEP"]').click()
-                cy.get('input[name="address-number"]').type(deliver.address.number)
-                cy.get('input[name="address-details"]').type(deliver.address.details)
-    
-                cy.get('input[name="address"]').should('have.value', deliver.address.street)
-                cy.get('input[name="district"]').should('have.value', deliver.address.district)
-                cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
-    
-                cy.contains('.delivery-method li', deliver.delivery_method).click()
-    
-                cy.get('input[accept^="image"]').attachFile('/images/' + deliver.cnh)
-    
-                cy.get('form button[type="submit"]').click()
-    
-                cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
-                    
+                
     
             })    
 
