1f50d6044ef9f4d042b774a6dae76c9678cdc6b7
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-10-12 17:09:07 -0300

    Export default new Page

--- cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
+++ cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -1,35 +1,14 @@
-import SignupPage from '../pages/SignupPage'
+import signup from '../pages/SignupPage'
 
-describe('cadastro', ()=>{
+describe('cadastro', () => {
 
-    before(function() {
-        cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os cados de testes')
-    })
-
-    beforeEach(function(){
-        cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    })
-
-    after(function() {
-        cy.log('Tudo aqui é executado uma única vez DEPOIS de TODOS os cados de testes')
-    })
-
-    afterEach(function(){
-        cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    })
-
-    it('Usuário deve ser tornar um entregador', ()=>{
-        cy.viewport(1440, 900)
-        cy.visit('https://buger-eats.vercel.app')
-
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+    it('Usuário deve ser tornar um entregador', () => {
 
         var deliver = {
             name: 'João Ricardo',
             cpf: '00942438592',
             email: 'joaodasilva@bol.com.br',
-            whatsapp:'21742854783',
+            whatsapp: '21742854783',
             address: {
                 postalcode: '24120191',
                 street: 'Alameda São Boaventura',
@@ -38,49 +17,46 @@ describe('cadastro', ()=>{
                 district: 'Fonseca',
                 city_state: 'Niterói/RJ'
             },
-            delivery_method:'Moto',
-            cnh:'cnh-digital.jpg'
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg'
         }
 
-        var signup = new SignupPage()
+
 
         signup.go()
         signup.fillform(deliver)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-        signup.modalContentShouldBe(expectedMessage) 
-
-        })
-
-        it('CPF incorreto', ()=>{
-              
-            var deliver = {
-                name: 'João Ricardo',
-                cpf: '123456789aa',
-                email: 'joaodasilva@bol.com.br',
-                whatsapp:'21742854783',
-                address: {
-                    postalcode: '24120191',
-                    street: 'Alameda São Boaventura',
-                    number: '1000',
-                    details: 'apto 1050',
-                    district: 'Fonseca',
-                    city_state: 'Niterói/RJ'
-                },
-                delivery_method:'Moto',
-                cnh:'cnh-digital.jpg'
-            }
-            var signup = new SignupPage()
-
-            signup.go()
-            signup.fillform(deliver)
-            signup.submit()
-            signup.alertMessageShoulBe('Oops! CPF inválido')
-
-    
-                
-    
-            })    
+        signup.modalContentShouldBe(expectedMessage)
 
     })
+
+    it('CPF incorreto', () => {
+
+        var deliver = {
+            name: 'João Ricardo',
+            cpf: '123456789aa',
+            email: 'joaodasilva@bol.com.br',
+            whatsapp: '21742854783',
+            address: {
+                postalcode: '24120191',
+                street: 'Alameda São Boaventura',
+                number: '1000',
+                details: 'apto 1050',
+                district: 'Fonseca',
+                city_state: 'Niterói/RJ'
+            },
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+
+        signup.go()
+        signup.fillform(deliver)
+        signup.submit()
+        signup.alertMessageShoulBe('Oops! CPF inválido')
+
+
+    })
+
+})
--- cypress-discovery/buger-eats/cypress/pages/SignupPage.js
+++ cypress-discovery/buger-eats/cypress/pages/SignupPage.js
@@ -44,4 +44,4 @@ class SignupPage {
 
 }
 
-export default SignupPage;    
+export default new SignupPage;    
