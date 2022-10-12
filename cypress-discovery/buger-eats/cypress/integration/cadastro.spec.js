410ac711a4de5d1551711c0f6077058518f805ab
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-10-12 11:46:00 -0300

    Massa de testes

--- cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
+++ cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -8,5 +8,17 @@ describe('cadastro', ()=>{
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
+        var entregador = {
+            nome: 'João Ricardo',
+            cpf: '012458247521',
+            email: 'joaodasilva@bol.com.br',
+            whatsapp:'21742854783',
+        }
+
+            cy.get('input[ name="name"]').type(entregador.nome)
+            cy.get('input[ name="cpf"]').type(entregador.cpf)
+            cy.get('input[ name="email"]').type(entregador.email)
+            cy.get('input[ name="whatsapp"]').type(entregador.whatsapp)
+        })
+
     })
-})
