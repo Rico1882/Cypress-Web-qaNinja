474e369768b1674363db9bfb65d2ed5be55d0539
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-10-12 11:04:21 -0300

    atualizacao

---
+++ cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -0,0 +1,12 @@
+
+
+describe('cadastro', ()=>{
+    it('Usuário deve ser tornar um entregador', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+    })
+})
