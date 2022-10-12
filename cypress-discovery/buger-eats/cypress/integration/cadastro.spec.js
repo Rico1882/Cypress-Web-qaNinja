5c7c44c81fba472274da4e41205c2bc54eae67df
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-10-12 17:36:45 -0300

    Trabalhando com Fixtures

--- cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
+++ cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -2,61 +2,25 @@ import signup from '../pages/SignupPage'
 
 describe('cadastro', () => {
 
-    it('Usuário deve ser tornar um entregador', () => {
+    beforeEach(function () {
+        cy.fixture('deliver').then(function (d) {
+            this.deliver = d
+        })
+    })
 
-        var deliver = {
-            name: 'João Ricardo',
-            cpf: '00942438592',
-            email: 'joaodasilva@bol.com.br',
-            whatsapp: '21742854783',
-            address: {
-                postalcode: '24120191',
-                street: 'Alameda São Boaventura',
-                number: '1000',
-                details: 'apto 1050',
-                district: 'Fonseca',
-                city_state: 'Niterói/RJ'
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
-
-
-
+    it('Usuário deve ser tornar um entregador', function () {
         signup.go()
-        signup.fillform(deliver)
+        signup.fillform(this.deliver.signup)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         signup.modalContentShouldBe(expectedMessage)
-
     })
 
-    it('CPF incorreto', () => {
-
-        var deliver = {
-            name: 'João Ricardo',
-            cpf: '123456789aa',
-            email: 'joaodasilva@bol.com.br',
-            whatsapp: '21742854783',
-            address: {
-                postalcode: '24120191',
-                street: 'Alameda São Boaventura',
-                number: '1000',
-                details: 'apto 1050',
-                district: 'Fonseca',
-                city_state: 'Niterói/RJ'
-            },
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
-
+    it('CPF incorreto', function () {
         signup.go()
-        signup.fillform(deliver)
+        signup.fillform(this.deliver.cpf_inv)
         signup.submit()
         signup.alertMessageShoulBe('Oops! CPF inválido')
-
-
     })
-
 })
