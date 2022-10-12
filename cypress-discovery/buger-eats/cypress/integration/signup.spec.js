5b5d581e43ca020891c312de5dbb3519368c8fff
Joao Ricardo Soares da Silva <98894124+Rico1882@users.noreply.github.com>
2022-10-12 20:45:40 -0300

    New ITs On The Block (Step by step)

--- cypress-discovery/buger-eats/cypress/integration/signup.spec.js
+++ cypress-discovery/buger-eats/cypress/integration/signup.spec.js
@@ -5,9 +5,9 @@ import signupPage from '../pages/SignupPage'
 describe('Signup', () => {
 
     //beforeEach(function () {
-      //  cy.fixture('deliver').then(function (d) {
-        //    this.deliver = d
-       // })
+    //  cy.fixture('deliver').then(function (d) {
+    //    this.deliver = d
+    // })
     // })
 
     it('User shold be deliver', function () {
@@ -46,17 +46,33 @@ describe('Signup', () => {
         signup.alertMessageShoulBe('Oops! Email com formato inválido.')
     })
 
-    it.only('Require fields', function(){
-        signupPage.go()
-        signupPage.submit()
-        signupPage.alertMessageShoulBe('É necessário informar o nome')
-        signupPage.alertMessageShoulBe('É necessário informar o CPF')
-        signupPage.alertMessageShoulBe('É necessário informar o email')
-        signupPage.alertMessageShoulBe('É necessário informar o CEP')
-        signupPage.alertMessageShoulBe('É necessário informar o número do endereço')
-        signupPage.alertMessageShoulBe('Selecione o método de entrega')
-        signupPage.alertMessageShoulBe('Adicione uma foto da sua CNH')
-    } )
-})
+    context('Require fields', function () {
+
+        const messages = [
+            { field: 'name', output: 'É necessário informar o nome' },
+            { field: 'cpf', output: 'É necessário informar o CPF' },
+            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'postalcode', output: 'É necessário informar o CEP' },
+            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'delivery_method', output: 'Selecione o método de entrega' },
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+        ]
+
+        before(function () {
+            signupPage.go()
+            signupPage.submit()
+
+        })
+
+        messages.forEach(function (msg) {
+            it(`${msg.field} is required`, function () {
+                signupPage.alertMessageShoulBe(msg.output)
+            })
+
+        })
+
+    })
+
+   })
 
 
